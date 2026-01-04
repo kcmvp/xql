25c4e331a96d3b6cf79f86f403c361632ca63967
kcmvp <kcheng.mvp@gmail.com>
2026-01-05 00:55:47 +0800

    delete unused file

--- internal/sealer.go
+++
@@ -1,8 +0,0 @@
-package internal
-
-// Sealer is an exported token type placed in the internal package so external
-// modules cannot import it. Requiring this type as a parameter to an
-// interface method prevents other modules from implementing that interface.
-// Packages inside the same module may import this package and therefore can
-// implement the sealed interface if needed.
-type Sealer struct{}
